9246b86d9643814666d49abbc7f470e033a3c26c
vasiliy <vash050@rambler.ru>
2024-06-29 21:56:38 +0300

    -run config

--- history-kareliy-app/main.py
+++ history-kareliy-app/main.py
@@ -12,4 +12,10 @@ app.include_router(
 )
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", reload=True)
+    uvicorn.run(
+        "main:app",
+        host=settings.run.host,
+        port=settings.run.port,
+        log_level="info",
+        reload=True
+    )
