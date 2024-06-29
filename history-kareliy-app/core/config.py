8d1e0ec1eb96cc31eba889cd1dd4a90944ceecc9
vasiliy <vash050@rambler.ru>
2024-06-29 22:40:48 +0300

    -db_helper -config

--- history-kareliy-app/core/config.py
+++ history-kareliy-app/core/config.py
@@ -1,18 +1,29 @@
+from pydantic import BaseModel
+from pydantic import PostgresDsn
 from pydantic_settings import BaseSettings
 
 
-class RunConfig(BaseSettings):
+class RunConfig(BaseModel):
     host: str = "0.0.0.0"
     port: int = 8000
 
 
-class ApiPrefix(BaseSettings):
+class ApiPrefix(BaseModel):
     prefix: str = "/api"
+
+
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    max_overflow: int = 10
+    pool_size: int = 50
 
 
 class Settings(BaseSettings):
     run: RunConfig = RunConfig()
     api_prefix: ApiPrefix = ApiPrefix()
+    db: DatabaseConfig
 
 
 settings = Settings()
