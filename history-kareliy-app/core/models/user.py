51d32ec374aa9ff95a85e155c2489a95a5a31933
vasiliy <vash050@rambler.ru>
2024-06-30 13:51:47 +0300

    - migrate user

--- history-kareliy-app/core/models/user.py
+++ history-kareliy-app/core/models/user.py
@@ -1,9 +1,15 @@
+from datetime import datetime
+
 from fastapi_users.db import (
-    SQLAlchemyBaseUserTable
+    SQLAlchemyBaseUserTable,
 )
+from sqlalchemy import Column, DateTime, func
+from sqlalchemy.orm import Mapped, mapped_column
 
 from core.models import Base
 
 
 class User(Base, SQLAlchemyBaseUserTable[int]):
-    pass
+    first_name: Mapped[str]
+    last_name: Mapped[str]
+    created_at= mapped_column(DateTime(), server_default=func.now())
