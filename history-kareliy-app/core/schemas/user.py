d302fd050a858152052ba19f11aa9c24647daf30
vasiliy <vash050@rambler.ru>
2024-06-30 16:47:19 +0300

    - add field in schemas

--- history-kareliy-app/core/schemas/user.py
+++ history-kareliy-app/core/schemas/user.py
@@ -1,15 +1,22 @@
+from datetime import datetime
+from typing import Optional
+
 from fastapi_users import schemas
 
 from core.types.user_id import UserIdType
 
 
 class UserRead(schemas.BaseUser[UserIdType]):
-    pass
+    first_name: str
+    last_name: str
+    created_at: Optional[datetime]
 
 
 class UserCreate(schemas.BaseUserCreate):
-    pass
+    first_name: str
+    last_name: str
 
 
 class UserUpdate(schemas.BaseUserUpdate):
-    pass
+    first_name: str
+    last_name: str
