eef88b5743122dcea81d68f5d77723c011dbd785
vasiliy <vash050@rambler.ru>
2024-06-30 14:17:27 +0300

    - get user db

--- history-kareliy-app/core/models/user.py
+++ history-kareliy-app/core/models/user.py
@@ -1,15 +1,23 @@
-from datetime import datetime
+from typing import TYPE_CHECKING
 
 from fastapi_users.db import (
     SQLAlchemyBaseUserTable,
+    SQLAlchemyUserDatabase,
 )
-from sqlalchemy import Column, DateTime, func
+from sqlalchemy import DateTime, func
 from sqlalchemy.orm import Mapped, mapped_column
 
 from core.models import Base
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
 
 
 class User(Base, SQLAlchemyBaseUserTable[int]):
     first_name: Mapped[str]
     last_name: Mapped[str]
-    created_at= mapped_column(DateTime(), server_default=func.now())
+    created_at = mapped_column(DateTime(), server_default=func.now())
+
+    @classmethod
+    def get_db(cls, session: "AsyncSession"):
+        return SQLAlchemyUserDatabase(session, User)
