e3f85839829a667a479f16af8f2416b0eef23429
vasiliy <vash050@rambler.ru>
2024-06-30 14:40:29 +0300

    - create model access_token

--- history-kareliy-app/core/models/access_token.py
+++ history-kareliy-app/core/models/access_token.py
@@ -1,19 +1,22 @@
-from fastapi import Depends
 from fastapi_users_db_sqlalchemy.access_token import (
     SQLAlchemyAccessTokenDatabase,
     SQLAlchemyBaseAccessTokenTable,
 )
+from sqlalchemy import Integer, ForeignKey
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import Mapped, mapped_column
 
 from core.models import Base
 from core.types.user_id import UserIdType
 
 
 class AccessToken(Base, SQLAlchemyBaseAccessTokenTable[UserIdType], ):
-    pass
+    user_id: Mapped[UserIdType] = mapped_column(
+        Integer,
+        ForeignKey("users.id", ondelete="cascade"),
+        nullable=False,
+    )
 
-
-async def get_access_token_db(
-        session: AsyncSession = Depends(get_async_session),
-):
-    yield SQLAlchemyAccessTokenDatabase(session, AccessToken)
+    @classmethod
+    def get_db(cls, session: "AsyncSession"):
+        yield SQLAlchemyAccessTokenDatabase(session, cls)
--- history-kareliy-app/core/models/user.py
+++ history-kareliy-app/core/models/user.py
@@ -21,4 +21,4 @@ class User(Base, SQLAlchemyBaseUserTable[UserIdType]):
 
     @classmethod
     def get_db(cls, session: "AsyncSession"):
-        return SQLAlchemyUserDatabase(session, User)
+        return SQLAlchemyUserDatabase(session, cls)
