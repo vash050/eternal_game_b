287f7c86fdd25383f3bc3e7679f34d2f77079cbd
vasiliy <vash050@rambler.ru>
2024-06-30 14:42:30 +0300

    - if TYPE_CHECKING:

--- history-kareliy-app/core/models/access_token.py
+++ history-kareliy-app/core/models/access_token.py
@@ -1,13 +1,17 @@
+from typing import TYPE_CHECKING
+
 from fastapi_users_db_sqlalchemy.access_token import (
     SQLAlchemyAccessTokenDatabase,
     SQLAlchemyBaseAccessTokenTable,
 )
 from sqlalchemy import Integer, ForeignKey
-from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import Mapped, mapped_column
 
 from core.models import Base
 from core.types.user_id import UserIdType
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
 
 
 class AccessToken(Base, SQLAlchemyBaseAccessTokenTable[UserIdType], ):
