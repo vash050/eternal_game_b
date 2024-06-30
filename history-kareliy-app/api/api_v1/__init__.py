45ace2a65ca1656ef8bfa1ed67c438d69b08479f
vasiliy <vash050@rambler.ru>
2024-06-30 22:45:04 +0300

    - add http bearer

--- history-kareliy-app/api/api_v1/__init__.py
+++ history-kareliy-app/api/api_v1/__init__.py
@@ -1,23 +1,24 @@
 from fastapi import (
     APIRouter,
-    # Depends,
+    Depends,
 )
 
-# from fastapi.security import HTTPBearer
+from fastapi.security import HTTPBearer
 
 from core.config import settings
 
 from .auth import router as auth_router
 
 from .users import router as users_router
+
 # from .messages import router as messages_router
 
 
-# http_bearer = HTTPBearer(auto_error=False)
+http_bearer = HTTPBearer(auto_error=False)
 
 router = APIRouter(
-    prefix=settings.api_prefix.v1.prefix
-    # dependencies=[Depends(http_bearer)],
+    prefix=settings.api_prefix.v1.prefix,
+    dependencies=[Depends(http_bearer)],
 )
 router.include_router(auth_router)
 router.include_router(users_router)
