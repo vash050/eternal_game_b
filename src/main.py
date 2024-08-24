aa120d90f188fde599ec8f57e903deb5eb1496eb
vasiliy <vash050@rambler.ru>
2024-08-24 12:33:37 +0300

    - Cors

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from contextlib import asynccontextmanager
 
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from core.config import settings
 
@@ -21,6 +22,25 @@ main_app = FastAPI(
     lifespan=lifespan,
 )
 main_app.include_router(api_router, prefix=settings.api_prefix.prefix)
+
+# TODO: настроить CORS
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:5173/",
+    "http://192.168.0.18:8000",
+    "http://0.0.0.0:8000",
+]
+
+main_app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     uvicorn.run(
